b875d24626b500cb2fd7c5823c2619dfeb1f44c0
Jacob Rothberg <67800788+jacobrothberg@users.noreply.github.com>
2020-11-10 16:11:51 -0600

    extracting NLP based features, part 2

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,14 +1,24 @@
-import nltk
+from nltk import word_tokenize
+from nltk import pos_tag
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+
+wnl = WordNetLemmatizer()
+
 
 class FeatureExtractor:
 
-    def __init__(self):
+    def __init__(self, sentence):
+        self.sentence = sentence
 
-        self.tokens = list()
+        self.tokens = word_tokenize(self.sentence)
 
-    def tokenize(self, sentence):
-        self.tokens = nltk.word_tokenize(sentence)
+        self.lemmas = [wnl.lemmatize(token) for token in self.tokens]
 
-    def get_tokens(self, sentence):
+        self.pos_tags = pos_tag(self.tokens)
 
-        return self.tokens
+        self.dict_of_synsets = {}
+
+        self.synset_dict = dict()
+        for token in self.tokens:
+            self.synset_dict = {**self.synset_dict, token : wordnet.synsets(token)}
