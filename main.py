7ceb9ecc93f63c7d46cbbcfc47e7cd6c6408a596
poorn <naidu.poorna10@gmail.com>
2020-11-29 17:13:27 -0600

    printing the scores

--- main.py
+++ main.py
@@ -33,21 +33,27 @@ if __name__ == '__main__':
     featureExtractor_test = FeatureExtractor()
     new_dataset_test = featureExtractor_test.getFeatures(dataset_test)
 
-    i = 0
-    labelMappings = {}
-    for label in dataset_train['labels']:
-        if label not in labelMappings.keys():
-            labelMappings[label] = i
-            i += 1
+    labelMappings = {'Other': 0,'Instrument-Agency(e1,e2)': 1, 'Instrument-Agency(e2,e1)': 2,
+                     'Cause-Effect(e1,e2)': 3, 'Cause-Effect(e2,e1)': 4,'Member-Collection(e1,e2)': 5,
+                     'Member-Collection(e2,e1)' : 6,'Entity-Destination(e1,e2)': 7, 'Entity-Destination(e2,e1)': 8,
+                     'Content-Container(e1,e2)': 9, 'Content-Container(e2,e1)': 10,'Message-Topic(e1,e2)': 11,
+                     'Message-Topic(e2,e1)': 12, 'Product-Producer(e1,e2)': 13,'Product-Producer(e2,e1)': 14,
+                     'Entity-Origin(e1,e2)': 15, 'Entity-Origin(e2,e1)': 16,'Component-Whole(e1,e2)': 17,
+                     'Component-Whole(e2,e1)': 18}
 
-    print(len(labelMappings))
-    print(labelMappings)
+    classToLabel = {0: ('Other','NA'),1: ('Instrument-Agency','(e1,e2)'), 2: ('Instrument-Agency','(e2,e1)'),
+                    3: ('Cause-Effect','(e1,e2)'),4: ('Cause-Effect','(e2,e1)'),5: ('Member-Collection','(e1,e2)'),
+                    6: ('Member-Collection','(e2,e1)'),7: ('Entity-Destination','(e1,e2)'),8: ('Entity-Destination','(e2,e1)'),
+                    9: ('Content-Container','(e1,e2)'),10: ('Content-Container','(e2,e1)'),11: ('Message-Topic','(e1,e2)'),
+                    12: ('Message-Topic','(e2,e1)'),13: ('Product-Producer','(e1,e2)'),14: ('Product-Producer','(e2,e1)'),
+                    15: ('Entity-Origin','(e1,e2)'),16: ('Entity-Origin','(e2,e1)'),17: ('Component-Whole','(e1,e2)'),
+                    18: ('Component-Whole','(e2,e1)')}
+
 
     tokenizer,embedding_matrix, max_length, major_dep, word_index, x_text_seq, x_mut_ancestors_list, x_dependency_list_filter, x_label = transform(labelMappings,new_dataset_train,train = True)
     (_,_,_,_,_,y_text_seq,y_mut_ancestors_list,y_dependency_list_filter,y_label) = transform(labelMappings,new_dataset_test,tokenizer,max_length,major_dep,word_index,train=False)
 
-    print("X: ", x_text_seq.shape,x_mut_ancestors_list.shape,x_dependency_list_filter.shape,x_label.shape)
-    print("Y: ", y_text_seq.shape, y_mut_ancestors_list.shape, y_dependency_list_filter.shape, y_label.shape)
+
 
     model, history = train_model(x_text_seq, x_mut_ancestors_list,
                                   x_dependency_list_filter, x_label,
@@ -63,4 +69,20 @@ if __name__ == '__main__':
     class_pred = np.argmax(prediction, axis=1)
     class_true = np.argmax(y_label, axis=1)
     conf = confusion_matrix(class_true, class_pred)
-    precision_recall_fscore = precision_recall_fscore_support(class_true, class_pred, average='macro')
+    precision,recall,fscore,support = precision_recall_fscore_support(class_true, class_pred, average='macro')
+    print("precision: ",precision*100,"\nrecall: ",recall*100,"\nfscore: ",fscore*100)
+
+    y_true = [classToLabel[x] for x in class_true]
+    y_pred = [classToLabel[x] for x in class_pred]
+
+    cor_rel_cor_edge, cor_rel_wr_edge = 0, 0
+    for i in range(len(y_true)):
+        if y_true[i][0] == y_pred[i][0]:
+            if y_true[i][1] == y_pred[i][1]:
+                cor_rel_cor_edge += 1
+            else:
+                cor_rel_wr_edge += 1
+
+    print("Correct relation,correct edges: ",100 * cor_rel_cor_edge/ len(y_true))
+    print("Correct relation, wrong edge: ", 100 * cor_rel_wr_edge / len(y_true))
+
